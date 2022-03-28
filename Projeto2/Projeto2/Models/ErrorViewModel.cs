d17ac62fa66797c1d79e3bbb5fd166cdd3368aee
Tiago Fernandes <aluno22102@ipt.pt>
2022-03-28 14:15:24 +0100

    V1

---
+++ Projeto2/Projeto2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projeto2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
